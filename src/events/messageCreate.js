60154616a11662b1131f83b78e0158fc8b540df4
Tejasri Addanki <tejasriaddanki3@gmail.com>
2026-01-31 07:18:35 +0000

    Remove obsolete allergen handler and clean command responsibilities

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,10 +1,6 @@
-import storeAllergens from "../commands/storeAllergens.js";
 
 export default {
   name: "messageCreate",
   async execute(message) {
-    if (message.author.bot) return;
-
-    storeAllergens.execute(message);
   },
 };
